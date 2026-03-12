2b4060ceb44aec07264b5e7a1aab9e05fc006a9e
DevishreeS-hub <sdevishree18@gmail.com>
2026-03-12 21:46:56 +0530

    lvl3half

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level_AnxietyForest");
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Level_AnxietyForest");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game Quit");
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,14 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    // Load the first level when Play button is pressed
     public void PlayGame()
     {
         SceneManager.LoadScene("Level_AnxietyForest");
     }
 
+    // Quit the game when Quit button is pressed
     public void QuitGame()
     {
-        Application.Quit();
         Debug.Log("Game Quit");
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Stops play mode in Unity
+        #else
+        Application.Quit(); // Quits the built game
+        #endif
     }
 }
